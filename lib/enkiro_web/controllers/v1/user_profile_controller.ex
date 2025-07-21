5ea3bc43f3aecb787b11a356ca1692f41d0bb569
Lockhinator <cameronl.lockhart@gmail.com>
2025-07-20 21:29:54 -0700

    added edit password endpoint (#11)

--- lib/enkiro_web/controllers/v1/user_profile_controller.ex
+++ lib/enkiro_web/controllers/v1/user_profile_controller.ex
@@ -2,6 +2,7 @@ defmodule EnkiroWeb.V1.UserProfileController do
   use EnkiroWeb, :controller
 
   alias Enkiro.Accounts.User
+  alias Enkiro.Accounts
 
   def show_me(conn, _params) do
     # Guardian has already loaded the user into the conn for us
@@ -11,7 +12,17 @@ defmodule EnkiroWeb.V1.UserProfileController do
 
   def update_me(conn, %{"user" => user_params}) do
     with %User{} = current_user <- Guardian.Plug.current_resource(conn),
-         {:ok, user} <- Enkiro.Accounts.update_user(current_user, user_params) do
+         {:ok, user} <- Accounts.update_user(current_user, user_params) do
+      render(conn, "show.json", %{user: user})
+    end
+  end
+
+  def update_password_me(conn, %{
+        "user" => %{"current_password" => current_password, "new_password" => new_password}
+      }) do
+    with %User{} = current_user <- Guardian.Plug.current_resource(conn),
+         {:ok, user} <-
+           Accounts.update_user_password(current_user, current_password, %{password: new_password}) do
       render(conn, "show.json", %{user: user})
     end
   end
